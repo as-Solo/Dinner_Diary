d3500ab3a7fa29bb43ef72dd5ac57f1a7e12f997
as-Solo <asdelsolo@gmail.com>
2021-05-20 10:35:22 +0200

    Heroku

--- sql/pruebas_sql.sql
+++ sql/pruebas_sql.sql
@@ -23,8 +23,19 @@ WHERE restaurantes.nombre = 'Restaurante Italiano San Marcos' AND restaurantes.d
 
 SELECT *
 FROM restaurantes
-WHERE restaurantes.nombre = "O'Lagar"
+WHERE restaurantes.nombre = "Casa Enriqueta"
 
+SELECT *
+FROM restaurantes
+WHERE restaurantes.busquedas = 2
+
+INSERT INTO restaurantes (restaurantes.busquedas)
+VALUES (0)
+WHERE restaurantes.nombre = "Casa Enriqueta"
+
+UPDATE restaurantes
+SET restaurantes.busquedas = 0
+        
 SELECT *
 FROM etiquetas
 WHERE etiquetas.id_etiqueta = 8553
@@ -41,3 +52,11 @@ INNER JOIN restaurantes
 ON restaurantes.id_rest = visitado.id_rest
 
 WHERE usuarios.id_usuario = 1  
+
+DELETE FROM visitado
+WHERE visitado.id_usuario = 26 AND visitado.comentario = "No sé ni dónde está"
+
+
+SELECT visitado.comentario
+FROM visitado
+WHERE visitado.id_usuario = 26
